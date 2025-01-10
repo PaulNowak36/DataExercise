45a26490fbace850765299188e657db6590e1990
PaulNowak36 <91249965+PaulNowak36@users.noreply.github.com>
2025-01-10 09:55:47 +0100

    class created

---
+++ frenchFries.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+class frenchFriesStand {       
+  public:             
+    string name;        
+    string city;  
+    string owner_Name;
+
+    int budget;
+    int fries_Stock;
+    int nb_Employees;
+
+    frenchFriesStand(string x, string y, string z, int a, int b, int c) { // Constructor with parameters
+      name = x;
+      city = y;
+      owner_Name = z;
+      budget = a;
+      fries_Stock = b;
+      nb_Employees = c;
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include "frenchFries.cpp"
+
 using namespace std;
 
 int main()
 {
+    frenchFriesStand biloute("Friterie a momo", "Point-a-Pitre", "Momo Jr.", 20000, 500, 6);
+
     cout << "PINGAS" << endl;
+    cout << "My fav french stand is: " << biloute.name << endl;
+    cout << "Its location is in: " << biloute.city << endl;
+    cout << "The owner name is: " << biloute.owner_Name << endl;
+    cout << "It has a budget of : " << biloute.budget << " euros" << endl;
+    cout << "It has a fries stock of : " << biloute.fries_Stock << " kilos." << endl;
+    cout << "Its number of employees is : " << biloute.nb_Employees << endl;
+
+
+
+
 }
